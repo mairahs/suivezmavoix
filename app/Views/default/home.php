26899585a00717fe15b9397472281d496e3b40e2
= <=>
2016-12-06 18:49:45 +0100

    Ajout front v1.0

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -1,7 +1,109 @@
-<?php $this->layout('layout', ['title' => 'Accueil']) ?>
+<?php $this->layout('layout', ['title' => 'Wf3 | Suivez ma voix']) ?>
 
 <?php $this->start('main_content') ?>
-	<h2>Let's code.</h2>
-	<p>Vous avez atteint la page d'accueil. Bravo.</p>
-	<p>Et maintenant, RTFM dans <strong><a href="../docs/tuto/" title="Documentation de W">docs/tuto</a></strong>.</p>
+	
+
+<!-- *****************************
+
+
+ 	Partie de la Vue de l'acceuil du site
+
+
+ ********************************** -->
+
+ 		
+	<!-- Contenu des vues  -->
+
+
+
+	<main>
+ 			<!-- Section haut de page (Image plus titre) -->
+
+
+ 		<!-- Artiste Image plus description   -->
+ 		<section>
+
+	 		<span class="imgSec1">
+
+	 					<img src="<?= $this->assetUrl('img/default.jpg') ?>" alt=""  title="image Artistes" >
+	 					<p class="desc">description image 1</p>
+	 		</span>
+
+	 		<span class="imgSec1">
+
+	 					<img src="<?= $this->assetUrl('img/default.jpg') ?>" alt=""  title="image Artistes" >
+	 					<p class="desc">description image 2</p>
+	 		</span>
+
+	 		<span class="imgSec1">
+
+	 					<img src="<?= $this->assetUrl('img/default.jpg') ?>" alt=""  title="image Artistes" >
+	 					<p class="desc">description image 3</p>
+	 		</span>
+
+	 		<span class="imgSec1">
+
+	 					<img src="<?= $this->assetUrl('img/default.jpg') ?>" alt=""  title="image Artistes" >
+	 					<p class="desc">description image 4</p>
+	 		</span>
+
+
+ 		</section>
+
+ 		<section>
+
+ 					<h2>Les artistes </h2>
+
+ 					<div class="imgSec2">
+
+ 						<img src="<?= $this->assetUrl('img/default.jpg') ?>" alt=""  title="image Artistes" >	 		
+
+ 						<img src="<?= $this->assetUrl('img/default.jpg') ?>" alt=""  title="image Artistes" >	 		
+
+ 						<img src="<?= $this->assetUrl('img/default.jpg') ?>" alt=""  title="image Artistes" > 		
+
+ 						<img src="<?= $this->assetUrl('img/default.jpg') ?>" alt=""  title="image Artistes" >
+
+ 						<img src="<?= $this->assetUrl('img/default.jpg') ?>" alt=""  title="image Artistes" >
+
+ 						<img src="<?= $this->assetUrl('img/default.jpg') ?>" alt=""  title="image Artistes" >
+ 		
+					</div>
+
+					<a href="" class="btn">Découvrir les artistes</a>
+
+ 		</section>
+
+ 		<section>
+ 				
+ 				<h2>Prochains évenements</h2>
+
+ 				<div class="imgSec3">
+
+ 				<img src="<?= $this->assetUrl('img/default_map.jpg') ?>">
+
+
+ 				<img src="<?= $this->assetUrl('img/default_map.jpg') ?>" alt=""  title="image Artistes" >
+
+ 				<img src="<?= $this->assetUrl('img/default_map.jpg') ?>" alt=""  title="image Artistes" >
+
+ 				<img src="<?= $this->assetUrl('img/default_map.jpg') ?>" alt=""  title="image Artistes" >
+ 					
+ 				</div>
+
+ 		</section>
+
+	</main>
+
+
+
+	<!-- *****************************
+
+
+	 	Fin Vue de l'acceuil du site
+
+
+	 ********************************** -->
+
+
 <?php $this->stop('main_content') ?>
--- app/Views/layout.php
+++ app/Views/layout.php
@@ -1,23 +1,100 @@
-<!DOCTYPE html>
-<html lang="fr">
+<!doctype html>
+<html>
 <head>
-	<meta charset="UTF-8">
+	<meta  charset="utf-8" />
 	<title><?= $this->e($title) ?></title>
-
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0" />
 </head>
+
 <body>
-	<div class="container">
+
+	<!-- *****************************
+
+
+	 		Bar de navigation laterale
+
+
+	 ********************************** -->
+
+		<aside>
+
+			<div id="imgW">
+				<img src="<?= $this->assetUrl('img/logoWf3.png') ?>">
+			</div>
+
+			<nav>
+				<ul>
+
+					<li class="active"><a href="<?php echo $this->url('default_home'); ?>">Accueil</a></li>
+					<li><a href="#">A propos</a></li>
+					<li><a href="#">Les artistes</a></li>
+					<li><a href="#">Les évenements du jour</a></li>
+					<li><a href="#">Nous contacter</a></li>
+
+				</ul>
+			</nav>
+
+
+			<div class="btnRs">
+
+				<a href="facebook.fr"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+				<a href="twitter.fr"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+				<a href="plus.google.com"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+
+			</div>
+
+		</aside>
+
 		<header>
-			<h1>W :: <?= $this->e($title) ?></h1>
+
+		<span class="bars">
+			<i class="fa fa-bars" aria-hidden="true"></i>
+		</span>
+
+			<div id="titre">
+				<h1>SUIVEZ MA VOIX !</h1>
+					<h2>Phrase d'acroche</h2>
+			</div>
+
+			<span id="connexion">
+
+				<!-- <a href="<?php echo $this->url('login'); ?>">Connexion</a> -->
+
+				<a href="inscription.php">Inscription</a>
+
+			</span>
+
 		</header>
 
-		<section>
-			<?= $this->section('main_content') ?>
-		</section>
+
+		<!-- *****************************
+
+
+		 	 Vue de l'acceuil du site
+
+
+		 ********************************** -->
+
+		<?= $this->section('main_content') ?>
+
+
+
+		<!-- *****************************
+
+
+		 	Fin Vue de l'acceuil du site
+
+
+		 ********************************** -->
 
 		<footer>
+			Copyrigth Wf3 | 2016 &copy
 		</footer>
-	</div>
-</body>
-</html>
+
+
+		<script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="<?php echo $this->assetUrl('js/flashmessage.js') ?>"></script>
+	</body>
+	</html>
